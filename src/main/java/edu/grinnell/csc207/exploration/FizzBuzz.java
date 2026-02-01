5ce5bcf6a794864bf50bb89172d676f0e9d8e453
rinn10 <rinrint2105@gmail.com>
2026-02-01 14:16:59 -0600

    FiBz

--- src/main/java/edu/grinnell/csc207/exploration/FizzBuzz.java
+++ src/main/java/edu/grinnell/csc207/exploration/FizzBuzz.java
@@ -1,13 +1,33 @@
 package edu.grinnell.csc207.exploration;
 
-public class FizzBuzz {
+public class FizzBuzz{
+    public static void fizzbuzz(int n){
+        String output="";
+        for(int i=1; i<n+1; i++){
+            if(i%3==0 && i%5==0){
+                output += "fizzbuzz";
+            }
+            else if(i%3==0){
+                output += "fizz";
+            }
+            else if(i%5==0){
+                output += "buzz";
+            }
+            else{
+                output += i;
+            }
+        System.out.println(output);
+        output="";
+        }
+    }
+
     // N.B., to run this program, use the following maven command to specify
     // this file as the program entry point rather than the class specified in
     // the pom.xml file:
     //
     // mvn compile exec:java -q "-Dexec.mainClass=edu.grinnell.csc207.exploration.FizzBuzz"
-    public static void main(String[] args) {
-        // TODO: fill me in!
-        System.out.println("Hello world!");   
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        fizzbuzz(n);
     }
 }
